ad72c73ef4e01cd28e11f346dfc0bae433cf28e1
Mohammadamin594 <mohammadamin.109000@gmail.com>
2025-11-26 10:31:58 +0530

    UPDATED REF MODEL WITH WORKING ALL PORT

--- emac_tx2rx_ref_model.sv
+++ emac_tx2rx_ref_model.sv
@@ -10,17 +10,22 @@
 
 class emac_tx2rx_ref_model extends uvm_scoreboard;
     `uvm_component_utils(emac_tx2rx_ref_model)
-    
+    int tdata_ref;
     // Analysis Imp declarations
     `uvm_analysis_imp_decl(_tx_port3)
     `uvm_analysis_imp_decl(_tx_port4)
     `uvm_analysis_imp_decl(_tx_port5)
+    `uvm_analysis_imp_decl(_tdata_port0)
+    `uvm_analysis_imp_decl(_tdata_port1)
+    `uvm_analysis_imp_decl(_tdata_port2)
 
     emac_tx2rx_reg_block ral; 
     
     bit [15:0] valid_etypes[$] = '{16'h0800, 16'h8100};
     string port_names[NUM_PORTS] = '{"PORT3", "PORT4", "PORT5"};
-
+    axi_str_mas_seq_item #(32,32) axis_tdata_port0[$];
+    axi_str_mas_seq_item #(32,32) axis_tdata_port1[$];
+    axi_str_mas_seq_item #(32,32) axis_tdata_port2[$];
     // Queue Declarations
     emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) tx_mac_port3[$];
     emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) tx_mac_port4[$];
@@ -42,11 +47,18 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
     uvm_analysis_imp_tx_port3 #(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH), emac_tx2rx_ref_model) port0_imp;
     uvm_analysis_imp_tx_port4 #(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH), emac_tx2rx_ref_model) port1_imp;
     uvm_analysis_imp_tx_port5 #(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH), emac_tx2rx_ref_model) port2_imp;
+    // Imp Instantiation
+    uvm_analysis_imp_tx_port3 #(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH), emac_tx2rx_ref_model) port0_tdata_imp;
+    uvm_analysis_imp_tx_port4 #(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH), emac_tx2rx_ref_model) port1_tdata_imp;
+    uvm_analysis_imp_tx_port5 #(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH), emac_tx2rx_ref_model) port2_tdata_imp;
     //PORTS FOR SCOREBOARD
 	 uvm_analysis_port #(emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH)) expected_port3;
 	 uvm_analysis_port #(emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH)) expected_port4;
 	 uvm_analysis_port #(emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH)) expected_port5;
 
+    uvm_analysis_imp_tdata_port0#(axi_str_mas_seq_item #(32,32), emac_tx2rx_ref_model) tdata_port0;
+    uvm_analysis_imp_tdata_port1#(axi_str_mas_seq_item #(32,32), emac_tx2rx_ref_model) tdata_port1;
+    uvm_analysis_imp_tdata_port2#(axi_str_mas_seq_item #(32,32), emac_tx2rx_ref_model) tdata_port2;
     // Statistics
     int etype_drop[NUM_PORTS];
     int payload_drop[NUM_PORTS];
@@ -64,11 +76,20 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
         expected_port3 = new ("expected_port3", this);
         expected_port4 = new ("expected_port4", this);
         expected_port5 = new ("expected_port5", this);
+        tdata_port0 = new ("tdata_port0",this);
+        tdata_port1 = new ("tdata_port1",this);
+        tdata_port2 = new ("tdata_port2",this);
+
     endfunction
 
     function void build_phase (uvm_phase phase);
         super.build_phase (phase);
-        ral = emac_tx2rx_reg_block::type_id::create("ral"); 
+        ral = emac_tx2rx_reg_block::type_id::create("ral");
+        if (!uvm_config_db#(int)::get(this,"","ref_type",tdata_ref)) begin
+    		`uvm_fatal(get_full_name(), "set ref_mod type from env");
+        end else begin
+        $display("REF_TTPE=%d",tdata_ref);
+        end
     endfunction
 
     task run_phase(uvm_phase phase);
@@ -87,9 +108,70 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
     endfunction
 
     // -----------------------------------------------------------------
-    // WRITE FUNCTIONS (With Restored Logging)
+    // WRITE FUNCTIONS 
     // -----------------------------------------------------------------
-    virtual function void write_tx_port3(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) pkt_port3);
+
+ virtual function void write_tdata_port0(axi_str_mas_seq_item #(32,32) axis_tdata);
+  static int port0_pkt_count = 0;  
+  
+  axis_tdata_port0.push_back(axis_tdata);
+  port0_pkt_count++;
+   
+  $display("tdata_pkt_at_port3=%0d", port0_pkt_count);
+  $display("tdata inside ref model - PORT0");
+  axis_tdata.print(); 
+  
+  `uvm_info("DEBUG", "=== Component Hierarchy Debug PORT0 ===", UVM_LOW)
+  `uvm_info("DEBUG", $sformatf("Instance name: %s", get_name()), UVM_LOW)
+  `uvm_info("DEBUG", $sformatf("Full name: %s", get_full_name()), UVM_LOW)  
+  `uvm_info("DEBUG", $sformatf("Type name: %s", get_type_name()), UVM_LOW)
+  `uvm_info(get_full_name(), $sformatf("Got AXI-S Pkt in ref: %s", axis_tdata.sprint()), UVM_LOW)    
+  `uvm_info("QUEUE_STATUS", $sformatf("PORT0 queue size: %0d", axis_tdata_port0.size()), UVM_HIGH)
+endfunction
+
+virtual function void write_tdata_port1(axi_str_mas_seq_item #(32,32) axis_tdata_p1);
+  static int port1_pkt_count = 0;
+
+  axis_tdata_port1.push_back(axis_tdata_p1);
+  port1_pkt_count++;  // Increment counter
+  
+  $display("tdata inside ref model - PORT1");
+  axis_tdata_p1.print();  
+  $display("tdata_pkt_at_port4=%0d", port1_pkt_count);
+  
+  `uvm_info("DEBUG", "=== Component Hierarchy Debug PORT1 ===", UVM_LOW)
+  `uvm_info("DEBUG", $sformatf("Instance name: %s", get_name()), UVM_LOW)
+  `uvm_info("DEBUG", $sformatf("Full name: %s", get_full_name()), UVM_LOW)  
+  `uvm_info("DEBUG", $sformatf("Type name: %s", get_type_name()), UVM_LOW)
+  `uvm_info(get_full_name(), $sformatf("Got AXI-S Pkt in ref: %s", axis_tdata_p1.sprint()), UVM_LOW)  
+  
+  // Optional: Show queue status
+  `uvm_info("QUEUE_STATUS", $sformatf("PORT1 queue size: %0d", axis_tdata_port1.size()), UVM_HIGH)
+endfunction
+ 
+virtual function void write_tdata_port2(axi_str_mas_seq_item #(32,32) axis_tdata_p2);
+  static int port2_pkt_count = 0; 
+  
+  axis_tdata_port2.push_back(axis_tdata_p2);
+  port2_pkt_count++;
+  
+  // Display only the NEW packet that just arrived
+  $display("tdata inside ref model - PORT2");
+  axis_tdata_p2.print();
+  $display("tdata_pkt_at_port2 = %d", port2_pkt_count);
+  
+  `uvm_info("DEBUG", "=== Component Hierarchy Debug PORT2 ===", UVM_LOW)
+  `uvm_info("DEBUG", $sformatf("Instance name: %s", get_name()), UVM_LOW)
+  `uvm_info("DEBUG", $sformatf("Full name: %s", get_full_name()), UVM_LOW)  
+  `uvm_info("DEBUG", $sformatf("Type name: %s", get_type_name()), UVM_LOW)
+  `uvm_info(get_full_name(), $sformatf("Got AXI-S Pkt in ref port 2: %s", axis_tdata_p2.sprint()), UVM_LOW)
+  
+  // Display queue status (optional)
+  `uvm_info("QUEUE_STATUS", $sformatf("PORT2 queue size: %0d", axis_tdata_port2.size()), UVM_HIGH)
+endfunction
+
+virtual function void write_tx_port3(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) pkt_port3);
+  if(!tdata_ref)begin
         pkt_in_port[PORT3_IDX]++;
         tx_mac_port3.push_back(pkt_port3); 
         
@@ -103,14 +185,18 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
         foreach (tx_mac_port3[i]) begin
             $display("inside ref model");
             tx_mac_port3[i].print();
-            // Note: Changed .VLAN_ID to .vlan_id to match class definition
             $display("VLAN ID =%h", tx_mac_port3[i].vlan_id); 
             `uvm_info("REF_MODEL", $sformatf("%s: Converting MAC frame: %s", 
                               port_names[PORT3_IDX], tx_mac_port3[i].convert2string()), UVM_MEDIUM)
         end
+  end
+  else begin
+     $display("TDATA REF SELECTED");
+end
     endfunction
 
     virtual function void write_tx_port4(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) pkt_port4);
+  if(!tdata_ref)begin
         pkt_in_port[PORT4_IDX]++;
         tx_mac_port4.push_back(pkt_port4); 
         
@@ -124,9 +210,14 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
              `uvm_info("REF_MODEL", $sformatf("%s: Converting MAC frame: %s", 
                               port_names[PORT4_IDX], tx_mac_port4[i].convert2string()), UVM_MEDIUM)
         end
+      end
+          else begin
+             $display("TDATA REF SELECTED");
+      end
     endfunction
 
     virtual function void write_tx_port5(emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) pkt_port5);
+  if(!tdata_ref)begin
         pkt_in_port[PORT5_IDX]++;
         tx_mac_port5.push_back(pkt_port5); 
         
@@ -140,6 +231,11 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
              `uvm_info("REF_MODEL", $sformatf("%s: Converting MAC frame: %s", 
                               port_names[PORT5_IDX], tx_mac_port5[i].convert2string()), UVM_MEDIUM)
         end
+    end
+          else begin
+             $display("TDATA REF SELECTED");
+      end
+
     endfunction
 
     // -----------------------------------------------------------------
@@ -164,6 +260,7 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
                     valid_pkt_etype_port3.push_back(tx_mac_port3[i]);
                 end else begin
                     etype_drop[PORT3_IDX]++;
+                    axis_tdata_port0.delete();
                     `uvm_info("INVALID ETYPE", $sformatf("%s: EType=%h - DROPPED", port_names[PORT3_IDX], tx_mac_port3[i].e_type), UVM_LOW);
                 end
             end
@@ -228,6 +325,7 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
                     `uvm_info("[RETURN VALID E MAC PKT]","PORT 3",UVM_LOW)
                 end else begin
                     payload_drop[PORT3_IDX]++;
+                    axis_tdata_port0.delete();
                     `uvm_info("INVALID PAYLOAD SIZE", $sformatf("%s: PAYLOAD SIZE=%d - DROPPED", port_names[PORT3_IDX], valid_pkt_etype_port3[i].payload.size()), UVM_LOW);
                 end
             end
@@ -305,6 +403,7 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
                         valid_conn_cfg_port3.push_back(valid_mac_pkt_port3[i]);
                         `uvm_info("CONNECTION VALID", $sformatf("%s: CONN_VALID=%0d", port_names[PORT3_IDX], conn_valid_bit), UVM_LOW);
                     end else begin
+                      axis_tdata_port0.delete();
                         invalid[PORT3_IDX]++;
                         `uvm_info("CONNECTION INVALID", $sformatf("%s: CONN_VALID=%0d", port_names[PORT3_IDX], conn_valid_bit), UVM_LOW);
                     end
@@ -379,14 +478,16 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
     task vcid_fatch();
        fork
           vcid_fatch_port3();
-          // Add ports 4 and 5 here if needed
+          vcid_fatch_port4();
+          vcid_fatch_port5();
        join_none
     endtask
 
     task vcid_fatch_port3();
         emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) expected_pkt;
         emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) current_pkt;
-        
+        axi_str_mas_seq_item #(32,32) expected_tdata;  
+        axi_str_mas_seq_item #(32,32) current_tdata;  
         bit [14:0] cnn_addr;
         bit [2:0]  port_id = 3;
         bit [4:0]  vcid_addr;
@@ -398,28 +499,163 @@ class emac_tx2rx_ref_model extends uvm_scoreboard;
             wait(valid_conn_cfg_port3.size() > 0);
 
             current_pkt = valid_conn_cfg_port3.pop_front(); 
-
+          //  current_tdata = axis_tdata_port0.pop_front();
             cnn_addr = {port_id, current_pkt.vlan_id}; 
             
             if (ral.conn_config_reg_h[cnn_addr] != null) begin
                 conn_id_field = ral.conn_config_reg_h[cnn_addr].conn_id;
                 vcid_addr     = conn_id_field.get();
                 
-                `uvm_info("VCID REG ADDR", $sformatf ("VCID_ADDR=%h", vcid_addr), UVM_LOW) // Restored
+                `uvm_info("VCID REG ADDR", $sformatf ("VCID_ADDR_PORT3=%h", vcid_addr), UVM_LOW) // Restored
                 
                 if (ral.vcid_config_reg_h[vcid_addr] != null) begin
                     vcid_field = ral.vcid_config_reg_h[vcid_addr].vcid;
                     vcid       = vcid_field.get();
                     $display("REF_VCID=%h",vcid); 
-                    `uvm_info("VCID", $sformatf ("VCID=%h", vcid), UVM_LOW) // Restored
+                    `uvm_info("VCID", $sformatf ("VCID_PORT3=%h", vcid), UVM_LOW) // Restored
 
                     expected_pkt = get_rx_expected(current_pkt, vcid);
                     expected_port3.write(expected_pkt);
+                //    expected_tdata = expected_tdata_pkt(axis_tdata_port0,vcid);
+                end
+            end
+       end
+    endtask
+    task vcid_fatch_port4();
+        emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) expected_pkt_p4;
+        emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) current_pkt_p4;
+        axi_str_mas_seq_item #(32,32) expected_tdata_p4;  
+        axi_str_mas_seq_item #(32,32) current_tdata;  
+        bit [14:0] cnn_addr;
+        bit [2:0]  port_id = 4;
+        bit [4:0]  vcid_addr;
+        bit [7:0]  vcid;
+        uvm_reg_field conn_id_field;
+        uvm_reg_field vcid_field;
+
+        forever begin
+            wait(valid_conn_cfg_port4.size() > 0);
+
+            current_pkt_p4 = valid_conn_cfg_port4.pop_front(); 
+          //  current_tdata = axis_tdata_port0.pop_front();
+            cnn_addr = {port_id, current_pkt_p4.vlan_id}; 
+            
+            if (ral.conn_config_reg_h[cnn_addr] != null) begin
+                conn_id_field = ral.conn_config_reg_h[cnn_addr].conn_id;
+                vcid_addr     = conn_id_field.get();
+                
+                `uvm_info("VCID REG ADDR", $sformatf ("VCID_ADDR_PORT4=%h", vcid_addr), UVM_LOW) // Restored
+                
+                if (ral.vcid_config_reg_h[vcid_addr] != null) begin
+                    vcid_field = ral.vcid_config_reg_h[vcid_addr].vcid;
+                    vcid       = vcid_field.get();
+                    $display("REF_VCID=%h",vcid); 
+                    `uvm_info("VCID", $sformatf ("VCID_PORT4=%h", vcid), UVM_LOW) // Restored
+
+                    expected_pkt_p4 = get_rx_expected(current_pkt_p4, vcid);
+                    expected_port4.write(expected_pkt_p4);
+                  //  expected_tdata_p4 = expected_tdata_pkt(axis_tdata_port0,vcid);
                 end
             end
        end
     endtask
 
+    task vcid_fatch_port5();
+        emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) expected_pkt_p5;
+        emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) current_pkt_p5;
+        axi_str_mas_seq_item #(32,32) expected_tdata_p4;  
+        axi_str_mas_seq_item #(32,32) current_tdata;  
+        bit [14:0] cnn_addr;
+        bit [2:0]  port_id = 5;
+        bit [4:0]  vcid_addr;
+        bit [7:0]  vcid;
+        uvm_reg_field conn_id_field;
+        uvm_reg_field vcid_field;
+
+        forever begin
+            wait(valid_conn_cfg_port5.size() > 0);
+
+            current_pkt_p5 = valid_conn_cfg_port5.pop_front(); 
+          //  current_tdata = axis_tdata_port0.pop_front();
+            cnn_addr = {port_id, current_pkt_p5.vlan_id}; 
+            
+            if (ral.conn_config_reg_h[cnn_addr] != null) begin
+                conn_id_field = ral.conn_config_reg_h[cnn_addr].conn_id;
+                vcid_addr     = conn_id_field.get();
+                
+                `uvm_info("VCID REG ADDR", $sformatf ("VCID_ADDR_PORT5=%h", vcid_addr), UVM_LOW) // Restored
+                
+                if (ral.vcid_config_reg_h[vcid_addr] != null) begin
+                    vcid_field = ral.vcid_config_reg_h[vcid_addr].vcid;
+                    vcid       = vcid_field.get();
+                    $display("REF_VCID=%h",vcid); 
+                    `uvm_info("VCID", $sformatf ("VCID_PORT5=%h", vcid), UVM_LOW) // Restored
+
+                    expected_pkt_p5 = get_rx_expected(current_pkt_p5, vcid);
+                    expected_port5.write(expected_pkt_p5);
+                  //  expected_tdata_p4 = expected_tdata_pkt(axis_tdata_port0,vcid);
+                end
+            end
+       end
+    endtask
+
+
+/*function axi_str_mas_seq_item #(32,32) expected_tdata_pkt(axi_str_mas_seq_item #(32,32) original_tdata, bit[7:0] vcid);
+    axi_str_mas_seq_item #(32,32) modified_tdata;
+    bit [7:0] byte_stream[$];
+    bit [15:0] original_tci;
+    
+    // Clone the original transaction
+    modified_tdata = original_tdata.clone();
+    
+    // Convert tdata_q to byte stream for easier manipulation
+    { >> { byte_stream }} = { >> { original_tdata.tdata_q }};
+    
+    `uvm_info("BYTE_STREAM_DEBUG", $sformatf("Original byte stream size: %0d", byte_stream.size()), UVM_HIGH)
+    
+    // The frame structure in bytes:
+    // Bytes 0-5: DA (6 bytes)
+    // Bytes 6-11: SA (6 bytes) 
+    // Bytes 12-13: TCI (2 bytes) - we want to replace this with VCID (1 byte)
+    // Bytes 14-15: EType (2 bytes)
+    // Bytes 16+: Payload
+    
+    if (byte_stream.size() >= 16) begin  // At least DA+SA+TCI+EType
+        // Remove the 2-byte TCI (bytes 12-13)
+        byte_stream.delete(12);  // Delete byte 12 (TCI high byte)
+        byte_stream.delete(12);  // Delete byte 12 (TCI low byte) - now it's the same position
+        
+        // Insert 1-byte VCID at position 12
+        byte_stream.insert(12, vcid);
+        
+        `uvm_info("FRAME_MODIFY", 
+                  $sformatf("Replaced TCI with VCID=%0h, New frame length: %0d bytes", 
+                           vcid, byte_stream.size()), 
+                  UVM_LOW)
+    end else begin
+        `uvm_error("FRAME_TOO_SHORT", $sformatf("Frame too short for VCID insertion. Size: %0d bytes", byte_stream.size()))
+        return null;
+    end
+    
+    // Convert byte stream back to tdata_q (32-bit words)
+    modified_tdata.tdata_q.delete();
+    { >> { modified_tdata.tdata_q }} = { >> { byte_stream }};
+    
+    // Adjust tkeep_q for the new frame size (1 byte shorter)
+    modified_tdata.tkeep_q.delete();
+    foreach (modified_tdata.tdata_q[i]) begin
+        if (i == modified_tdata.tdata_q.size() - 1) begin
+            // Last word might have partial bytes
+            int bytes_in_last_word = byte_stream.size() % 4;
+            if (bytes_in_last_word == 0) bytes_in_last_word = 4;
+            modified_tdata.tkeep_q.push_back(bytes_in_last_word);
+        end else begin
+            modified_tdata.tkeep_q.push_back(4'hF); // All bytes valid
+        end
+    end
+    
+    return modified_tdata;
+endfunction*/
     function emac_rx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) get_rx_expected(
         emac_tx_seqs_item#(PAYLOAD_DATA_WIDTH,FRAME_DATA_WIDTH) valid_expected_pkt,
         bit [7:0] vcid
