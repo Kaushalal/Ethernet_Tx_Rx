2b8ad9989dc0dc7600da27b1b6defe3d6f5547ad
ThakurMuskan27 <thakurmuskan644@gmail.com>
2025-12-02 14:39:56 +0530

    Deleted unessesary folders

--- emac_tx2rx_protocol/AXI_4_Lite_VIP_f/AXI_4_Lite_VIP/ENV/axi_lite_define.sv
+++
@@ -1,6 +0,0 @@
-`ifndef AXI_LITE_DEFINE
-`define AXI_LITE_DEFINE
-
-typedef enum logic {AXI_LITE_WRITE,AXI_LITE_READ} trans_kind_e;
-   
-`endif
--- emac_tx2rx_protocol/AXI_Stream_VIP_f/AXI_Stream_VIP/ENV/axis_define.sv
+++
@@ -1,7 +0,0 @@
-`ifndef  AXIS_DEFINE_SV
-`define AXIS_DEFINE_SV
-
-`define DATA_WIDTH 32
-`define USER_WIDTH 32
-
-`endif
