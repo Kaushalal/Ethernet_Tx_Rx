7040c6b8362db7a67088abda7b05169d4fd2a1d8
ThakurMuskan27 <thakurmuskan644@gmail.com>
2025-12-03 11:58:48 +0530

    Merged sanity vseqs and test

--- E_mac_tx_to_rx_cluster_verification/TEST/mac_to_axi_s_test_pkg.svh
+++ E_mac_tx_to_rx_cluster_verification/TEST/mac_to_axi_s_test_pkg.svh
@@ -30,8 +30,7 @@ package mac_to_axi_s_test_pkg;
    import mac_to_axi_s_env_pkg::*;
   
   `include "mac_to_axi_s_base_test.svh"
-  `include "mac_tx_to_rx_sanity_vseqs.svh"
-  `include "mac_to_axi_s_sanity_test.svh"
+  `include "mac_tx_to_rx_sanity_test.svh"
   `include "mac_to_axi_s_payload_test.svh"
 
 
