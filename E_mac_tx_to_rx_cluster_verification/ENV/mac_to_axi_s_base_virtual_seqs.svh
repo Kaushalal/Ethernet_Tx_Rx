04505dd05d22daf2cf09c55580178c5481f1e72b
ThakurMuskan27 <thakurmuskan644@gmail.com>
2025-12-04 13:08:32 +0530

    Updated base virtual seqs

--- E_mac_tx_to_rx_cluster_verification/ENV/mac_to_axi_s_base_virtual_seqs.svh
+++ E_mac_tx_to_rx_cluster_verification/ENV/mac_to_axi_s_base_virtual_seqs.svh
@@ -18,7 +18,7 @@ class mac_to_axi_s_base_virtual_seqs extends uvm_sequence #(uvm_sequence_item);
   
   `uvm_declare_p_sequencer(mac_to_axi_s_virtual_seqr)
 
-   mac_tx_base_seqs mac_tx_seqs[int][];
+   mac_tx_base_seqs mac_tx_seqs[int][];   //// Associative arr -> in case u want to start multiple seqs on a sinple port ( ex- continousl toggling between valid and invalid scenarios 
    mac_tx_seqr mac_tx_seqr_h[];
    reg_conn_cfg_seq conn_cfg_seqs;
 
@@ -26,7 +26,7 @@ class mac_to_axi_s_base_virtual_seqs extends uvm_sequence #(uvm_sequence_item);
    rand bit [2:0]   temp_port_id;
                   
    rand bit         temp_connection_valid;
-   rand bit [4:0]   temp_connection_id[int][$];
+   rand bit [4:0]   temp_connection_id[$];
                   
    rand bit [3:0]   temp_out_port_sel;
    rand bit [31:0]  temp_crc_val;
@@ -60,29 +60,51 @@ class mac_to_axi_s_base_virtual_seqs extends uvm_sequence #(uvm_sequence_item);
 ////---------------------------------------------------------------------------------////
 ////              Function to collect connection id 
 ////---------------------------------------------------------------------------------////
-   function void collect_conn_id( bit[2:0] port_id, bit[4:0] conn_id );
-      temp_connection_id[port_id].push_back(conn_id);
+   function void collect_conn_id( bit[4:0] conn_id );
+      temp_connection_id.push_back(conn_id);
    endfunction
 
 ////---------------------------------------------------------------------------------////
-////--------------- We can use this function to configure the connection ------------////
-//// in_port_no -> use to select the input_port(1,2,3) ( by default 0 -> select any port ) || out_port_no -> used to select the output port(1,2,3) ( by defualt 0 -> select any port ); 
 ////---------------------------------------------------------------------------------////
-   task configure_connection( int in_port_no = 0, int out_port_no = 0, bit conn_valid_bit= 1, int no_of_configurations = 1);
+////--------------- We can use this function to configure the connection ------------////
+//// in_port_no -> use to select the input_port(1,2,3) ( by default 0 -> select any port ) || out_port_no -> used to select the output port(1,2,3) ( by defualt 0 -> select any port );
+////  range ( input argument ) 0 -> min_range; 1 -> in_bet_range; 2 -> max_range; 3 -> completely random 
+////---------------------------------------------------------------------------------////
+////---------------------------------------------------------------------------------////
+
+  virtual task configure_connection( int in_port_no = 0, int out_port_no = 0, bit conn_valid_bit= 1, int no_of_configurations = 1, bit collect_data = 1, bit[2:0] range_of_vlan=3, bit[2:0] range_of_conn_id=3 );
          
          repeat( no_of_configurations ) begin 
 
-            void'(conn_cfg_seqs.randomize with { (in_port_no == 1)->(port_id == 3); (in_port_no==2)->(port_id==4); (in_port_no==3)->(port_id==5); port_id inside {3,4,5}; connection_valid == conn_valid_bit ;(out_port_no==1)->(out_port_sel==8); (out_port_no==2)->(out_port_sel==9); (out_port_no==3)->(out_port_sel==10); out_port_sel inside {8,9,10}; !(connection_id inside {temp_connection_id[port_id]}) ; !(vlan inside {temp_vlan_q[port_id]}) ; crc_val == 32'h01020304;});
+            void'(conn_cfg_seqs.randomize with { 
+            (in_port_no == 1)->(port_id == 3); 
+            (in_port_no==2)->(port_id==4); 
+            (in_port_no==3)->(port_id==5);
+            port_id inside {3,4,5};
+            connection_valid == conn_valid_bit ;
+            (out_port_no==1)->(out_port_sel==8); 
+            (out_port_no==2)->(out_port_sel==9);
+            (out_port_no==3)->(out_port_sel==10); 
+            out_port_sel inside {8,9,10}; 
+            !(connection_id inside {temp_connection_id}) ; 
+            !(vlan inside {temp_vlan_q});
+            (range_of_vlan==0)->(vlan inside {[0:500]});
+            (range_of_vlan==1)->(vlan inside {[1000:2000]});
+            (range_of_vlan==2)->(vlan inside{[3095:4095]}); 
+            (range_of_conn_id==0)->(connection_id inside {[0:10]});
+            (range_of_conn_id==1)->(connection_id inside {[10:20]}); 
+            (range_of_conn_id==2)->(connection_id inside {[20:31]});         });
             
             conn_cfg_seqs.start(null);
             
+            if( collect_data == 1 ) begin 
             collect_vcid( conn_cfg_seqs.port_id,conn_cfg_seqs.connection_id, conn_cfg_seqs.vcid_val );
             collect_vlan_id( conn_cfg_seqs.port_id, conn_cfg_seqs.vlan );
-            collect_conn_id( conn_cfg_seqs.port_id, conn_cfg_seqs.connection_id );
+            collect_conn_id( conn_cfg_seqs.connection_id );
+            end 
             
-            if( temp_connection_id[conn_cfg_seqs.port_id].size == 32 )
-                 temp_connection_id[conn_cfg_seqs.port_id].delete();
-            `uvm_info("CONN_ID",$sformatf(" conn_id_q[%0d] = %p ",conn_cfg_seqs.port_id,temp_connection_id[conn_cfg_seqs.port_id]),UVM_DEBUG);
+            if( temp_connection_id.size == 32 )
+                 temp_connection_id.delete();
          end 
    endtask
 
